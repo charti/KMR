69b112b75f6442929714c5e4346b73747db4c680
Christian Chartron <chr.chart@gmail.com>
2015-06-21 16:43:33 +0200

    CalculationHelpers hinzugefügt

---
+++ KMR/Model/CalculationHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace KMR.Model
+{
+    public static class CalculationHelpers
+    {
+        public static Dictionary<string, double> CalcAnnuity()
+        {
+            var data = new Dictionary<string, double>();
+            return null;
+        }
+    }
+}
